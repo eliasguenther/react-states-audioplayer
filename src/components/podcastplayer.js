7e4312423e01acfedd93986bb906a585e4f4af18
Elias Günther <elias@elias.ninja>
2020-05-01 13:22:48 +0200

    added styling, emotion/css, framer motion, material icons

--- src/components/podcastplayer.js
+++ src/components/podcastplayer.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useMachine} from '@xstate/react';
 import { Machine, assign } from 'xstate';
 import { motion } from "framer-motion"
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
 
 
 const playerMachine = Machine({
@@ -79,7 +82,7 @@ const playerMachine = Machine({
     }
 });
 
-
+const standardboxsh = "0px 25px 55px rgba(0, 0, 0, 0.4)";
 
 
 //Actions for machine
@@ -111,8 +114,24 @@ const Podcastplayer = () => {
     
         if(current.value != "failure") {
         return (
-            <div className="player">
-                <audio controls ref={ref} onCanPlay={() => {              
+            <motion.div 
+            className="player" 
+            css={css`
+                width: 400px; 
+                height: 100px; 
+                height: 100px; 
+                position: relative;
+                border-radius: 4px;
+                overflow: hidden;
+                box-shadow: ${standardboxsh};
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 1rem;
+                box-sizing: border-box;
+                font-family: Montserrat, sans-serif;
+            `}>
+                <audio ref={ref} onCanPlay={() => {              
                     send("LOADED", {
                         audio: ref.current,
                     });
@@ -129,10 +148,11 @@ const Podcastplayer = () => {
                 >
                     <source src="https://d3ctxlq1ktw2nl.cloudfront.net/production/2020-3-21/66323909-44100-2-dcd47921d951c.mp3" type="audio/mpeg" />
                 </audio>
+                <Bar elapsed={elapsed} duration={duration} current={current} send={send}></Bar>
                 <Button current={current} send={send} />
-                <Timer elapsed={elapsed} duration={duration} />
-                <Bar elapsed={elapsed} duration={duration} current={current} send={send} />
-            </div>
+                <PodcastMeta elapsed={elapsed} duration={duration}  />
+                
+            </motion.div>
         ) } else {
             return("Diese Datei konnte nicht gefunden werden!")
         }
@@ -180,29 +200,94 @@ const calcAudiotime = (ctx, event) => {
 }
 
 
-const Timer = ({elapsed, duration}) => (
-    <div>
-        Elapsed: {minutes(elapsed)}:{seconds(elapsed)} <br/>
-        Duration: {(duration / 60).toFixed()}:{seconds(duration)} <br/>
-        Remaining: {remaining(elapsed, duration)}
+const PodcastMeta = ({elapsed, duration}) => (
+    <div
+    css={css`
+        position: relative;
+        user-select: none;
+        pointer-events: none;
+        height: 100%;
+        flex-grow: 1;
+        display: flex;
+        flex-flow: column;
+        justify-content: space-between;
+        padding-left: 1rem;
+
+    `}>
+        <div className="title">Titel der Episode</div>
+        <div 
+        css={css`
+            display: flex;
+            justify-content: space-between;
+        `}>
+            <span className="date">Datum</span>
+            <span className="duration">noch {remaining(elapsed, duration)}</span>
+        </div>
+        
+    
+        {/* Elapsed: {minutes(elapsed)}:{seconds(elapsed)} <br/>
+        Duration: {(duration / 60).toFixed()}:{seconds(duration)} <br/> */}
     </div>
+
 )
 
 const Bar = ({elapsed, duration, current, send}) => {
 
     return( 
-    <div onClick={() => send({type: "SCRUBBING", data: calcAudiotime(current, event)})} className="bar"  style={{ width: "200px", height: "20px", background: `linear-gradient(to right, grey 0%, grey ${percentage(elapsed, duration)}%, black ${percentage(elapsed, duration)}%, black 100%)`}}>
+    <div onClick={() => send({type: "SCRUBBING", data: calcAudiotime(current, event)})} className="bar"
+        css={css`
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: linear-gradient(to right, #CECECE 0%, #CECECE ${percentage(elapsed, duration)}%, #FFFFFF ${percentage(elapsed, duration) + .2}%, #FFFFFF 100%);
+        `}
+      >
+         
     </div>)
 }
 
 const Button = ({current, send}) => {
+    const generalCss = `
+        width: 4rem;
+        height: 4rem;
+        transform: translateY(0);
+        background: #F0F0F0;
+        border-radius: 10px;
+        border: 0;
+        user-select: none;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center
+        overflow: hidden;
+        svg{
+            transform-origin: 50% 50% !important;
+        }
+    `;
     if(current.matches({ready: "playing"})) {
         return(
-            <button onClick={() =>  send("PAUSE")  }>Pause</button>
+            <button 
+            css={css`
+                ${generalCss}
+            `} 
+            onClick={() =>  send("PAUSE")  }>
+                <motion.svg 
+                style={{scale: 1.5}}
+                whileHover={{ scale: 1.7 }}
+                whileTap={{ scale: 0.9 }} 
+                xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 0 24 24" width="35"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/><path d="M0 0h24v24H0z" fill="none"/></motion.svg></button>
         )
     } else {
         return(
-            <button onClick={() =>  send("PLAY")  }>Play</button>
+            <button onClick={() =>  send("PLAY")} css={css`${generalCss}`}>
+                <motion.svg 
+                style={{scale: 1.5, transformOrigin: "0px 0px"}} 
+                whileHover={{ scale: 1.7 }}
+                whileTap={{ scale: 0.9 }}  
+                xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 0 24 24" width="35"><path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/></motion.svg>
+            </button>
         )
     }
 }
